846f60becc6c38d0d86b9d420e17cc9940d73c8d
Adam Lind <adamsplind@gmail.com>
2025-10-28 19:25:43 -0600

    fix attempt

--- DotNetDependencies/obj/Debug/net8.0/DotNetDependencies.AssemblyInfo.cs
+++ DotNetDependencies/obj/Debug/net8.0/DotNetDependencies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24330f26c30beb4c86c9df9d308a8e7d6ab2b9d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c111669a0a4e5619a35311bd1e7325e2c26cd0e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
